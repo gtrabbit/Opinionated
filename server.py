e5600227be0a70fc214ef88833da8adbe890903f
Chris Rune <gtrabbit@gmail.com>
2017-09-05 17:37:17 -0400

    refactoring for debugging

--- server.py
+++ server.py
@@ -90,7 +90,9 @@ def gconnect():
         'tokeninfo?access_token={}'.format(
             access_token))
     h = httplib2.Http()
-    result = json.loads(h.request(url, 'GET')[1].decode('utf8'))
+    response = h.request(url, 'GET')[1].decode('utf8')
+    print(response)
+    result = json.loads(response)
     # If there was an error in the access token info, abort.
     if result.get('error') is not None:
         response = make_response(json.dumps(result.get('error')), 500)
